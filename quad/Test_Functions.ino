9ff3655731dc63befd61497ee04b855b06808ae4
Connor McCann <cjmccann@bu.edu>
2016-03-25 22:48:50 -0400

    25 Mar 2016

---
+++ quad/Test_Functions.ino
@@ -0,0 +1,9 @@
+//====================================================================================================
+//                                          Testing
+//====================================================================================================
+
+void testAltPID()
+{
+  // Use functions and write alternate control scheme to keep the quad hovering in a particular GPS column.
+}
+
