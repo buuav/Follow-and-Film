e3206853debe8c94ae5ebbbd46411fec4aebde2c
CJM2017 <cjmccann@bu.edu>
2016-04-04 19:55:00 -0400

    Delete Test_Functions.ino

--- quad/Test_Functions/Test_Functions.ino
+++
@@ -1,9 +0,0 @@
-//====================================================================================================
-//                                          Testing
-//====================================================================================================
-
-void testAltPID()
-{
-  // Use functions and write alternate control scheme to keep the quad hovering in a particular GPS column.
-}
-
